b274f94a42fb54948f4e6ecc7e437be90c1da5cb
Claystation <c.wijnekus@gmail.com>
2021-05-05 19:38:47 +0200

    Chapter 5 ac 1

---
+++ chapter5/ac5.01/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+type Weekday int
+const (
+	Monday Weekday = iota
+	Tuesday
+	Wednesday
+	Thursday
+	Friday
+	Saturday
+	Sunday
+)
+
+type employee struct {
+	id int
+	firstName string
+	lastName string
+}
+
+type developer struct {
+	individual employee
+	hourlyRate int
+	workWeek [7]int
+}
+
+func logHours(developer *developer, weekday Weekday, hours int) {
+	developer.workWeek[weekday] = hours
+}
+
+func hoursWorked(developer *developer) int {
+	total := 0
+	for _, v := range developer.workWeek {
+		total += v
+	}
+	return total
+}
+
+func main() {
+	dev := developer{employee{1, "Knoert", "Knabbelbaard"}, 15, [7]int{}}
+	logHours(&dev, Monday, 8)
+	logHours(&dev, Tuesday, 8)
+	fmt.Println("Hours worked on Monday: ", dev.workWeek[Monday])
+	fmt.Println("Hours worked on Tuesday: ", dev.workWeek[Tuesday])
+	fmt.Println("Hours worked this week: ", hoursWorked(&dev))
+}

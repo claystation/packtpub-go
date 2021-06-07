47b4f8eb4f6debd57511967ca53262e6c5518b43
Claystation <c.wijnekus@gmail.com>
2021-06-07 23:24:19 +0200

    Chapter 6 ac 3

---
+++ chapter6/ac6.03/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	ErrInvalidLastName = errors.New("invalid last name")
+	ErrInvalidRoutingNumber = errors.New("invalid routing number")
+)
+
+
+type directDeposit struct {
+	lastName string
+	firstName string
+	bankName string
+	routingNumber int
+	accountNumber int
+}
+
+func (d *directDeposit) validateRoutingNumber() error {
+	if d.routingNumber < 100 {
+		 panic(ErrInvalidRoutingNumber)
+	}
+	return nil
+}
+
+func (d *directDeposit) validateLastName() error {
+	if len(d.lastName) == 0 {
+		return ErrInvalidLastName
+	}
+	return nil
+}
+
+
+func main() {
+
+	d := directDeposit{
+		lastName: "",
+		firstName: "Abe",
+		bankName: "XYZ Inc",
+		routingNumber: 17,
+		accountNumber: 1809,
+	}
+
+	if err := d.validateLastName(); err != nil {
+		fmt.Println(err)
+	}
+
+	if err := d.validateRoutingNumber(); err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("*************************************")
+	fmt.Println("Last name:", d.lastName)
+	fmt.Println("First name:", d.firstName)
+	fmt.Println("Bank Name:", d.bankName)
+	fmt.Println("Routing Number:", d.routingNumber)
+	fmt.Println("Account Number:", d.accountNumber)
+
+}
